7df99b8e90ef55f98239a78570a3fc7310b48dde
DivyaGarg-22 <117917432+DivyaGarg-22@users.noreply.github.com>
2023-07-24 21:52:36 +0530

    Add files via upload

---
+++ Day_W4_23/program1.java
@@ -0,0 +1,11 @@
+class program1 {
+    public void flatten(TreeNode root) {
+        if (root == null)
+        return;
+    flatten(root.right);
+    flatten(root.left);
+    root.right = prev;
+    root.left = null;
+    prev = root;
+    }
+}
